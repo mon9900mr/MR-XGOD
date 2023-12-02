b87f1d6956e0aa4977bfd5f33fb364a5b6774153
xgod <102412706+mon9900mr@users.noreply.github.com>
2023-12-02 20:17:31 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+var a = 1
+function post8(){
+    var b = a+1
+    a = b
+    alert("number of downloads " + b)
+}
+var c = 1
+function post7(){
+    var d = c+1
+    c = d
+    alert("number of downloads " + d)
+}
+var e = 1
+function post6(){
+    var f = e+1
+    e = f
+    alert("number of downloads " + f)
+}
+var g = 1
+function post5(){
+    var h = g+1
+    g = h
+    alert("number of downloads " + h)
+}
+var i = 1
+function post4(){
+    var j = i+1
+    i = j
+    alert("number of downloads " + j)
+}
+var k = 1
+function post3(){
+    var l = k+1
+    k = l
+    alert("number of downloads " + l)
+}
+var m = 1
+function post2(){
+    var n = m+1
+    m = n
+    alert("number of downloads " + n)
+}
+var o = 1
+function post1(){
+    var q = o+1
+    o = q
+    alert("number of downloads " + q)
+}
