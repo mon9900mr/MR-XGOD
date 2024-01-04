b663f0855762eaa4ffb79a3d34fd80b3bca32c46
MR-XGOD <102412706+mon9900mr@users.noreply.github.com>
2024-01-04 20:14:59 +0330

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,24 @@
+var zero = 0
+function zero(){
+    
+    zero =+ 1
+}
+var dino = 0
+function dino(){
+    
+    dino =+ 1
+}
+var hero = 0
+function hero(){
+    
+    hero =+ 1
+}
+var blocks = 0
+function blocks(){
+    
+    blocks =+ 1
+}
+document.getElementById('zero').innerHTML = zero
+document.getElementById('dino').innerHTML = dino
+document.getElementById('hero').innerHTML = hero
+document.getElementById('blocks').innerHTML = blocks
