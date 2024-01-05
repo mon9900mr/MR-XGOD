b029d8af30f5097976f2ab98c6b3108e2b40e6d5
MR-XGOD <102412706+mon9900mr@users.noreply.github.com>
2024-01-05 15:15:50 +0330

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,43 @@
+var zero = 0
+function zero(){
+    zero =+1
+}
+var dino = 0
+function dino(){
+    
+    dino =+ 1
+}
+var hero = 0
+function hero(){
+    
+    hero =+ 1
+}
+var blocks = 0
+function blocks(){
+    
+    blocks =+ 1
+}
+var engine = 0
+function engine(){
+    engine =+ 1
+}
+var snake = 0
+function snake(){
+    snake =+ 1
+}
+var planet = 0
+function planet(){
+    planet =+ 1
+}
+var car = 0
+function car(){
+    car =+ 1
+}
+document.getElementById('zero').innerHTML = zero
+document.getElementById('dino').innerHTML = dino
+document.getElementById('hero').innerHTML = hero
+document.getElementById('blocks').innerHTML = blocks
+document.getElementById('engine').innerHTML = engine
+document.getElementById('snake').innerHTML = snake
+document.getElementById('planet').innerHTML = planet
+document.getElementById('car').innerHTML = car
